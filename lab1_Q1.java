4808b3aa6bf4a002a0ef4f25551176f9486f0db8
John Nyagaka <john.nyagaka@ashesi.edu.gh>
2019-01-18 17:09:18 +0000

    Lab1_Q1

---
+++ lab1_Q1.java
@@ -0,0 +1,15 @@
+package Practice;
+import java.util.*;
+
+public class lab1_Q3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner keyboard = new Scanner(System.in);
+		String input = keyboard.nextLine();
+		int strLength = input.length();
+		System.out.println(strLength);
+
+	}
+
+}
